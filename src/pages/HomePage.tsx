a246dfede2214bcdab6c5f6bd39ad3ac8327ef53
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-07-19 18:58:01 -0400

    Remove fleet preview from home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import ServicesOverview from '../components/ServicesOverview';
-import FleetPreview from '../components/FleetPreview';
 import WhyChooseUs from '../components/WhyChooseUs';
 import QuickQuote from '../components/QuickQuote';
 import Testimonials from '../components/Testimonials';
@@ -11,7 +10,6 @@ const HomePage: React.FC = () => {
     <div className="homepage">
       <HeroSection />
       <ServicesOverview />
-      <FleetPreview />
       <WhyChooseUs />
       <QuickQuote />
       <Testimonials />
